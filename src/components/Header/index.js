f047c3d917ac50e402c8f50d978fe95b91f40805
rvazmej <rogelio.vazquez@genomiclife.com>
2021-10-20 17:09:44 -0500

    Last TEKILA Version

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,15 @@
 import React from "react";
+import {
+  ShoppingBagIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 const Header = () => {
   return (
     <nav className="md:flex items-center justify-between flex-wrap  p-6">
       <h1 className="font-body text-3xl text-black mr-12"> Tekila </h1>
 
-      <div className="flex md:hidden">
-        <button className="flex items-center px-3 py-2 border rounded  cursor-pointer">
-          <svg
-            class="fill-current h-4 w-4"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
-
-      <div className="hidden lg:flex items-center justify-items-center space-x-10">
+      <div className="flex items-center justify-items-center space-x-10">
         <a href="google.com" class="py-4 px-2  text-black  font-serif ">
           Acerca de
         </a>
@@ -32,7 +24,6 @@ const Header = () => {
           class="py-4 px-2 text-black font-serif hover:text-green-500 transition duration-300"
         >
           Promociones
-
         </a>
         <a
           href="google.com"
@@ -48,49 +39,9 @@ const Header = () => {
         </a>
       </div>
       <div className="hidden lg:flex  items-center justify-center space-x-8 mr-40">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-          />
-        </svg>
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-          />
-        </svg>
+        <ShoppingBagIcon className="h-5 w-5 text-black" />
+        <SearchIcon className="h-5 w-5 text-black" />
+        <UserIcon className="h-5 w-5 text-black" />
       </div>
     </nav>
   );
